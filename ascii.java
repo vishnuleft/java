beaca649343c9bf8e9d90735574e0b87ba0daa59
vishnuleft <vishnugobi46@gmail.com>
2024-09-13 15:13:35 +0530

    Create ascii.java

---
+++ ascii.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class input_output{
+     public static void main (String argu[]){
+         int n;
+         Scanner sc=new Scanner(System.in);
+         char a=sc.next().charAt(0);
+         n=(int)a;
+         System.out.println("ascii value:"+n);
+     }
+}
